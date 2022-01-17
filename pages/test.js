711baf0bf7cc780bd74269a673a6d5ab23782f67
aayusshhh <bhardwajaayush0202@gmai.com>
2022-01-17 23:25:03 +0530

    link done

---
+++ pages/test.js
@@ -0,0 +1,16 @@
+// icons
+const twitter = "/icons/twitter.png"
+const instagram = "/icons/instagram.png"
+const facebook = "/icons/facebook.png"
+
+const Test = () => {
+  return (
+    <>
+   <a href="https://instagram.com/jaipurfoodjunction?utm_medium=copy_link"> <img src= {instagram} ></img></a>
+   <a href="https://www.facebook.com/jaipurfoodj/"> <img src= {facebook} ></img></a>
+   <a href="https://twitter.com/jaipur_food?t=_lMc3W6PY8-TSJz6iuoTRA&s=09"> <img src= {twitter} ></img></a>
+    </>
+  )
+}
+
+export default Test
---
+++ public/icons/test.js
@@ -0,0 +1,16 @@
+// icons
+const twitter = "/icons/twitter.png"
+const instagram = "/icons/instagram.png"
+const facebook = "/icons/facebook.png"
+
+const Test = () => {
+  return (
+    <>
+    <img src= {twitter}></img>
+    <img src= {instagram}></img>
+    <img src= {facebook}></img>
+    </>
+  )
+}
+
+export default Test
